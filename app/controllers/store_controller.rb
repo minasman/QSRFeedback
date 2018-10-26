8770af987cfae524caf8ce47ababad368e0f1e9b
github username <github email address>
2018-10-25 19:06:07 -0500

    set up skeleton

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,27 @@
+class StoreController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def comment_params
+        params.require('comment').permit(:)
+    end
+end
